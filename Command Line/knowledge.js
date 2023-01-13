eab64ff7feaac349eda69ba0243a9822850e8581
harrisonrazan <harrisonrazan@gmail.com>
2023-01-12 19:50:17 -0600

    Finished knowledge check

---
+++ Command Line/knowledge.js
@@ -0,0 +1,8 @@
+let firstName = 'harrison'
+let last_name = 'R'
+const currentYear = 2023
+let nextYear = currentYear + 1
+
+console.log(firstName + ' ' + last_name + ' ' + currentYear + ' ' + nextYear)
+
+console.log(currentYear >= nextYear)
