10154331bf6f6eab49dd03979961159f2bfd448e
harrisonrazan <harrisonrazan@gmail.com>
2023-01-17 11:36:54 -0600

    Finalized DGR

--- Command Line/knowledge.js
+++ Command Line/knowledge.js
@@ -6,3 +6,10 @@ let nextYear = currentYear + 1
 console.log(firstName + ' ' + last_name + ' ' + currentYear + ' ' + nextYear)
 
 console.log(currentYear >= nextYear)
+
+console.log(tempfunc2)
+
+const tempfunc = () => console.log('hello')
+const tempfunc2 = function() {
+    return 'hello2'
+}
