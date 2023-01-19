8f3cecde66e7ea1e2efbc2674952fbed43dbcadf
Brentjr1 <121051084+Brentjr1@users.noreply.github.com>
2023-01-19 13:29:42 -0500

    one

---
+++ Functions/review.js
@@ -0,0 +1,21 @@
+let num = 2
+console.log("num");
+
+function multiplyBy(num1, num2, num3) {
+    return num1 + num2, num3;
+}multiplyBy(2,5,7,);
+console.log("multiplyBy");
+
+let name = "Brent";
+let currentYear = 2023;
+num = currentYear + num;
+console.log("name"+ "currentYear");
+
+const name = (b) => b + 100;
+console.log("name");
+
+callCollect =() => { 'num', 'multiplyBy' , 'name' + 'currentYear'
+
+}
+
+function callCollect ()
