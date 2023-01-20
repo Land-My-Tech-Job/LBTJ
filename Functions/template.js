0f911cac2236abd72d556ee45759509f7e66ebd4
harrisonrazan <harrisonrazan@gmail.com>
2023-01-19 20:01:53 -0600

    added template.js

---
+++ Functions/template.js
@@ -0,0 +1,74 @@
+
+// ## Problem One
+
+// Age Calculator:
+//  * Store your birth year in a constant variable.
+//  * Store a future year in a variable.
+//  * Calculate your 2 possible ages for that year based on the stored values.
+//  * For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
+//  * Log age into console
+
+//  * Log them to the screen like so: "I will be either `ageMin` or `ageMax` in `futureYear`", substituting the values.
+
+// const birthYear = 1988
+// let futureYear = 2026
+// let age = futureYear - birthYear
+
+// console.log(age)
+
+// ## Problem Two
+
+// Snack Supply Calculator:
+//  * Store your current age in a variable.
+//  * Store a maximum age in a constant variable.
+//  * Store an estimated snack amount per day (as a number).
+//  * Calculate how many snacks you would eat total, from your current age until the maximum age.
+        // subtract maxAge by age and multiply the total by snackAmount
+        // store it lifetime snacks 
+
+//  * Log the result to the screen like this: "You will need `snackNumber` to last you until the age of `maxAge`".
+
+// let age = 33
+// const maxAge = 100
+// let snackAmount = 1
+
+// let lifetimeSnacks = (maxAge - age) * snackAmount
+
+// console.log("You will need" + lifetimeSnacks)
+
+// ## Problem Four
+
+// Temperature Converter:
+//  * Store a celsius temperature into a variable.
+//  * Convert it to fahrenheit and output "`tempInCelsius`°C is `tempInFahrenheit`°F".
+
+//  * Now store a fahrenheit temperature into a variable.
+//  * Convert it to celsius and output "`tempInFahrenheit`°F is `tempInCelsius`°C."
+
+// formula for conversion (temperature in celsius × 9/5) + 32 /// (32°F − 32) × 5/9 
+let tempInCelsius = 23
+let tempInFahrenheit = (tempInCelsius * 9/5) + 32
+let newTempinCelsius = (tempInFahrenheit - 32) * (5/9)
+
+console.log(tempInFahrenheit + ' ' + newTempinCelsius)
+
+
+
+/**
+ * Takes in two values and returns their rounded average
+ * 
+ * Hint: the way to get the average of a set of numbers is
+ * to sum all the numbers then divide that sum by how many numbers 
+ * you added
+ *
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number} - A whole number equal to the average of a and b
+ */
+let lifetimeSnacks = (maxAge - age) * snackAmount
+
+function average(a, b) {
+    return 
+}
+
+
